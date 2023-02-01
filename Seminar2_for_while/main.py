d97f6d1ec44a956fad9817ab5b044e53c27f53b1
Artem <burkun_artem@mail.ru>
2023-02-01 13:36:51 +0300

    первый

---
+++ Seminar2_for_while/main.py
@@ -0,0 +1,14 @@
+
+
+# a = 'hello'
+# for i in a:
+#     print(i, end='')
+#
+# for e in range(2, 10, 2): #(от, до, шаг) (10, 2, -1)
+#     print(e)
+
+# for ind in range(len(a)):
+#     print(a[ind])
+
+for _ in range(3):
+    print('HELLO')
---
+++ Seminar2_for_while/main5.py
@@ -0,0 +1,18 @@
+# Дано натуральное число A > 1. Определите, каким по
+# счету числом Фибоначчи оно является, то есть выведите
+# такое число n, что φ(n)=A. Если А не является числом Фибоначчи, выведите число -1
+
+first_el = 0
+second_el = 1
+number = int(input())
+next_el = first_el + second_el
+i = 3
+while next_el < number:
+    first_el = second_el
+    second_el = next_el
+    next_el = first_el + second_el
+    i += 1
+if next_el == number:
+    print(i)
+else:
+    print(-1)
