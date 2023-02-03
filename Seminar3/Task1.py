d51493089a3cf13509f9d3b80c683e004e1bf195
Artem <burkun_artem@mail.ru>
2023-02-03 13:24:10 +0300

    03022023 1323

--- Seminar3/Task1.py
+++ Seminar3/Task1.py
@@ -1,2 +1,6 @@
 # 17. Дан список чисел. Определите, сколько в нем встречается различных чисел.
 
+numbers = [11, 22, 33, 44, 55, 11, 22, 33, 11, 1]
+unique_numbers = set(numbers)
+print(len(unique_numbers))
+print(unique_numbers)
