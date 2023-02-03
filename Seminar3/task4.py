41f4ec6dcbc200874aaa2aba7caac92326419684
Artem <burkun_artem@mail.ru>
2023-02-03 13:26:38 +0300

    03022023 1326

--- Seminar3/task4.py
+++ Seminar3/task4.py
@@ -2,6 +2,10 @@
 # количество элементов массива, больших предыдущего (элемента с предыдущим номером).
 # list_1 = [0, -1, 5, 2, 3]
 
-array = [0, -1, 5, 2, 3]n = 0for i in range(1, len(array)):    if array[i] > array[i-1]:
-n += 1print(n)
+array = [0, -1, 5, 2, 3]
+n = 0
+for i in range(1, len(array)):
+    if array[i] > array[i-1]:
+        n += 1
+print(n)
 
