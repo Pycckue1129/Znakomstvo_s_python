408decd424601db89d2aa9fcd960e194636af73d
Artem <burkun_artem@mail.ru>
2023-02-08 11:45:32 +0300

    practika dictionary 08022023

--- Seminar4_slovari_mnozhestva/task4.py
+++ Seminar4_slovari_mnozhestva/task4.py
@@ -1,4 +1,4 @@
-my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20, 'g': 9999}
+# my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20, 'g': 999}
 
 # maxim = 0
 # for i in my_dict.values():
@@ -6,10 +6,31 @@ my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20, 'g': 999
 #     if i > maxim:
 #         maxim = i
 # print(maxim)
-
-maxim = 0
+my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20, 'g': 9999}
+maxim_value1 = 0
+maxim_value2 = 0
+maxim_value3 = 0
 for i in my_dict.items():
-    if maxim < i[1]:
-        maxim = i[1]
+    if maxim_value1 < i[1]:
+        maxim_value1 = i[1]
+        maxim_key1 = i[0]
+for i in my_dict.values():
+    if i[1] == maxim_value1:
+        del my_dict[i[0]]
 
-print(maxim)
+# for i in my_dict.items():
+#     if maxim_value2 < i[1]:
+#         maxim_value2 = i[1]
+#         maxim_key2 = i[0]
+# del my_dict[maxim_key2]
+#
+# for i in my_dict.items():
+#     if maxim_value3 < i[1]:
+#         maxim_value3 = i[1]
+#         maxim_key3 = i[0]
+# del my_dict[maxim_key3]
+#
+# print(maxim_value1, maxim_value2, maxim_value3)
+#
+# del my_dict[20]
+print(my_dict)
