1302656f191768c8da5c327ec6347789497e0972
Artem <burkun_artem@mail.ru>
2023-02-16 21:51:45 +0300

    seminar 160223

---
+++ Seminar7-function-higher-order/Main.py
@@ -0,0 +1,38 @@
+
+# some_list = [1, 2, 3, 4, 5, 6]
+# for ind in range(0, len(some_list)):
+#     some_list[ind] = str(some_list[ind])
+# print(some_list)
+
+
+# def square(x):
+#     return x ** 2
+#
+#
+# new_list = list(map(lambda x: x + 1, some_list))
+# print(new_list)
+
+# def even(x):
+#     return type(x) == int
+
+# new_list = list(filter(lambda x: type(x) == int, some_list))
+# print(new_list)
+
+
+# some_list = [10, 20, 30, 40]
+# some_dict = {}
+# print(list(enumerate(some_list)))
+# for ind, value in enumerate(some_list):
+#     some_dict[ind] = value
+#
+# print(some_dict)
+#
+# for ind in range(0, len(some_list)):
+#     print(ind, some_list[ind])
+
+
+# first_list = ('apple', 'orange', 'grape', 'лимон')
+# second_list = ['яблоко', 'апельсин', 'виноград']
+# print(list(zip(first_list, second_list)))
+# for eng, ru in zip(first_list, second_list):
+#     print(eng, ru)
