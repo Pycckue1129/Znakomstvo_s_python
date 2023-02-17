a7d48172061a543ace9b8f3b0ff292bd45b5ece8
Artem <burkun_artem@mail.ru>
2023-02-17 18:31:25 +0300

    DZ complete

--- Seminar7-function-higher-order/DZ/Task2.py
+++ Seminar7-function-higher-order/DZ/Task2.py
@@ -18,11 +18,7 @@
 #
 
 
-from math import log10
-
-
 def print_operation_table(operation, num_rows=6, num_columns=6):
-
     for row in range(1, num_rows + 1):
         for column in range(1, num_columns + 1):
             print(operation(row, column), end='\t')
